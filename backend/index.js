a16fe35c5416768964b863608588e1de384e47a6
raythx98 <65889977+raythx98@users.noreply.github.com>
2022-07-11 02:30:10 +0800

    Redirect from short URL

--- backend/index.js
+++ backend/index.js
@@ -17,9 +17,6 @@ app.use(express.json());
 app.post('/getlink', (req, res) => {
     const link = req.body.link;
 
-    // if exists in database, return
-
-    // doesn't exist in database
     const short_link = nanoid();
     db.query(
         'INSERT INTO links (link, shortened_link) VALUES (?,?)', 
@@ -28,10 +25,28 @@ app.post('/getlink', (req, res) => {
             if (err) {
                 console.log(err);
             } else {
+                console.log(result)
                 res.send(short_link)
             }
         }
     );
+})
+
+app.get('/:shortUrl', (req, res) => {
+    
+    db.query("SELECT link FROM links WHERE shortened_link = (?) LIMIT 1",
+    [req.params.shortUrl], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (result && result.length > 0) {
+                return res.redirect("//"+result[0].link);
+            } else {
+                return res.status(404).json('No url found');
+            }
+           
+        }
+    });
 })
 
 app.listen(3001, ()=> {
