274ca5288b89e942a7143e273a83e8328ad20a84
raythx98 <65889977+raythx98@users.noreply.github.com>
2022-07-10 22:07:43 +0800

    Add error and display links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,31 @@
 import "./App.css";
 import { useState } from "react";
-import Axios from 'axios';
+import Axios from "axios";
 
 function App() {
   const [link, setLink] = useState("");
+  const [linkError, setLinkError] = useState("Error");
+  const [newLink, setNewLink] = useState("");
+  const [visible, setVisible] = useState(true);
 
   const displayInfo = () => {
     console.log(link);
-  }
+  };
+
   const addLink = () => {
+    setLinkError("");
+    setVisible(true);
+    if (link.length < 1) {
+      setLinkError("Input link should not be empty");
+      return;
+    }
+    setVisible(false);
     console.log(link);
-    Axios.post(
-      'http://localhost:3001/getlink', {link: link}
-    ).then((res) => console.log(res));
-  }
+    Axios.post("http://localhost:3001/getlink", { link: link }).then((res) => {
+      console.log(res);
+      setNewLink(res.data)    
+    });
+  };
 
   return (
     <div className="App">
@@ -25,9 +37,15 @@ function App() {
             setLink(event.target.value);
           }}
         />
+        <div className="error">{linkError}</div>
         <button onClick={addLink}>Shorten URL</button>
-        <label>Shortened URL:</label>
       </div>
+      {visible ? null : (
+        <div className="newlink information">
+          <label>Your shortened link is: </label>
+          <p>{newLink}</p>
+        </div>
+      )}
     </div>
   );
 }
