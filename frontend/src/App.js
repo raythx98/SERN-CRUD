71225caa95d984c0e04e5b3fafd8801753697977
raythx98 <65889977+raythx98@users.noreply.github.com>
2022-07-10 19:47:37 +0800

    Initial React Styling

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="information">
+        <label>Input Link:</label>
+        <input type="link"/>
+        <button>Shorten URL</button>
+        <label>Shortened URL:</label>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
