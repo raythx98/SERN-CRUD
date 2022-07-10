f71fa803813012671ed21d1e74e90ad3db39f759
raythx98 <65889977+raythx98@users.noreply.github.com>
2022-07-10 21:20:57 +0800

    Frontend call API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,32 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Axios from 'axios';
 
 function App() {
+  const [link, setLink] = useState("");
+
+  const displayInfo = () => {
+    console.log(link);
+  }
+  const addLink = () => {
+    console.log(link);
+    Axios.post(
+      'http://localhost:3001/getlink', {link: link}
+    ).then((res) => console.log(res));
+  }
+
   return (
     <div className="App">
       <div className="information">
         <label>Input Link:</label>
-        <input type="link"/>
-        <button>Shorten URL</button>
+        <input
+          type="link"
+          onChange={(event) => {
+            setLink(event.target.value);
+          }}
+        />
+        <button onClick={addLink}>Shorten URL</button>
         <label>Shortened URL:</label>
-        
       </div>
     </div>
   );
